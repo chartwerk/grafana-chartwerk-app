cdfd54df97251cade3e21428fab1fbd589dd7f56
corpglory-dev <dev@corpglory.com>
2021-04-08 15:23:17 +0300

    minor fixes

--- src/timepicker.ts
+++ src/timepicker.ts
@@ -3,6 +3,7 @@ import { react2AngularDirective } from './react2angular';
 // @ts-ignore
 import { TimePicker } from '@grafana/ui';
 
+
 react2AngularDirective('timepicker', TimePicker, [
   // TODO: there are more props
   'value',
